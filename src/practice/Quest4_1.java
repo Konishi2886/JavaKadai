7b161a574238880c8127d9a0752b90bae8df85cb
jmcmedicalシステム <jmcmedical@J-PCN115>
2024-08-26 11:55:43 +0900

    プロジェクト作成

--- src/practice/Quest4_1.java
+++ src/practice/Quest4_1.java
@@ -12,3 +12,36 @@ public class Quest4_1 {
 		game.start();
 	}
 }
+
+//模範解答
+
+//package test4;
+//
+//import java.util.Scanner;
+//
+//public class Quest1 {
+//
+//	public static void main(String[] args) {
+//
+//		User com = new User("Com");
+//		User person = new User("person");
+//		Program program = new Program();
+//		
+//		Scanner input = new Scanner(System.in);
+//
+//		do {
+//
+//			program.Start();
+//			String personHand = input.next();
+//
+//			person.SetHand(personHand);
+//			com.SetHand("1");
+//
+//			program.Battle(person.Hand, com.Hand);
+//			program.end();
+//
+//		} while (!program.Finish);
+//
+//	}
+//
+//}
--- src/practice/Quest4_Player.java
+++ src/practice/Quest4_Player.java
@@ -57,3 +57,33 @@ public class Quest4_Player {
 		return hand;
 	}
 }
+
+//模範解答
+
+//package test4;
+//
+//public class User {
+//	public String Hand;
+//	
+//	public User(String user) {
+//		
+//	}
+//
+//	public void SetHand(String handNum) {
+//		
+//		switch (handNum) {
+//		case "0":
+//			Hand = "グー";
+//			break;
+//		case "1":
+//			Hand = "チョキ";
+//			break;
+//		case "2":
+//			Hand = "パー";
+//			break;
+//		default:
+//			System.out.println("正しい値が入力されませんでした。システムを終了します");
+//			System.exit(0);
+//		}			
+//	}
+//}
